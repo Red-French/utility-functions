a9700961fc2e4af993589ecbfaaa2bfa535922fc
Red-French <red@redfrench.net>
2016-01-21 23:52:10 -0600

    combined 'return' with reduce method for conciseness

--- rangeObj.js
+++ rangeObj.js
@@ -1,15 +1,14 @@
 module.exports = function (num) {
   var range = require('./range');  // required file is where range is calc'd and returned
   if (num) {  // if true (num exists)
-    var obj = range(num).reduce(function (all, num) {  // perform function in reqd module
+    return range(num).reduce(function (all, num) {  // perform function in reqd module
                                                        // and perform reduce method
     all[num] = num;  // set index and move item(num) into array
     return all;  // reduce requires something be returned
   },{})  // second parameter of the reduce method is an object (the starting point)
   }
   else {  // else if num === false
-    return {};  // return emtpy object
+    return {};  // return empty object
   }
-  return obj;  // return oject
 };
 
